4d30dac0d81ddfed913b9cc3f52e3dbeb8860605
Oktawian-L <oktawianlasek@gmail.com>
2018-08-15 21:32:29 +0200

    testing - working exaplme

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
+var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
 //anaonysmous function inside
 // przenosimy wszystko na klika
-btn.addEventListener("click",function())
+btn.addEventListener("click",function()
 {
     var ourRequest = new XMLHttpRequest();
     ourRequest.open(
         "GET",
         "https://learnwebcode.github.io/json-example/animals-1.json"
     );
-    ourRequest.onload= function(){
-    var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-    }; ourRequest.onload = function () {
+    ourRequest.onload = function () {
         var ourData = JSON.parse(ourRequest.responseText);
+        renderujHTML(ourData);
         console.log(ourData[0]);
     };
     //sending request
     ourRequest.send();
-};
-
-
-
-
-
-
-
+});
 //argumenty to co przekazujemy
 //paramtery to co w def feunkcja przyjumuje
+
+function renderujHTML(dane_input)
+{
+    animalContainer.insertAdjacentHTML('beforeend','testing 13w');
+}
