3f8f0298145ea15565e87bca5d45ca8a1424e88a
Oktawian-L <oktawianlasek@gmail.com>
2018-08-15 20:48:43 +0200

    JSON local data example

---
+++ main.js
@@ -0,0 +1,14 @@
+var thePets
+[
+    {
+        "name":"Meowsalot",
+        "species":"cat",
+        "favfood":"tuna"
+    },
+    {
+        "name": "Barky",
+        "species": "dog",
+        "favfood": "carrots"
+    }
+]
+// thePets[0].favfood
