291da2f147a6014f55f6bde455cee949fd781188
Oktawian-L <oktawianlasek@gmail.com>
2018-08-15 21:25:20 +0200

    action for button

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-var thePets
-[
-    {
-        "name":"Meowsalot",
-        "species":"cat",
-        "favfood":"tuna"
-    },
-    {
-        "name": "Barky",
-        "species": "dog",
-        "favfood": "carrots"
-    }
-]
-// thePets[0].favfood
+var btn = document.getElementById("btn");
+//anaonysmous function inside
+// przenosimy wszystko na klika
+btn.addEventListener("click",function())
+{
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open(
+        "GET",
+        "https://learnwebcode.github.io/json-example/animals-1.json"
+    );
+    ourRequest.onload= function(){
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
+    }; ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]);
+    };
+    //sending request
+    ourRequest.send();
+};
+
+
+
+
+
+
+
+//argumenty to co przekazujemy
+//paramtery to co w def feunkcja przyjumuje
